e2f1bfa0c17cc7cffe16e890bb9cc2c129d0dd0c
kevin miguta <migutak@gmail.com>
2019-12-02 16:10:32 +0300

    all good

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-const Nexmo = require('nexmo');
+// const Nexmo = require('nexmo');
 const bodyParser = require('body-parser');
 const express= require('express');
+const cors= require('cors');
 
 // init app
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 app.post('/', (req, res) => {
-    const from = req.body.from
-    const to = req.body.to;
-    const text = req.body.text
+    console.log(req.body)
+    const accountSid = req.body.accountSid;
+    const authToken = req.body.authToken;
+    const client = require('twilio')(accountSid, authToken);
 
-    const nexmo = new Nexmo({
+    client.messages
+    .create({
+        body: req.body.body,
+        from: req.body.from,
+        to: req.body.to
+    }, (err, response) => {
+        if(err) {
+            console.log(err);
+            res.json({
+                result: 'FAIL',
+                response: err
+            })
+        } else {
+            res.json({
+                result: 'OK',
+                response: response
+            })
+        }
+    })
+
+   /* const nexmo = new Nexmo({
         apiKey: req.body.api_key,
         apiSecret: req.body.api_secret,
       }, {debug: true});
@@ -27,8 +50,7 @@ app.post('/', (req, res) => {
             console.log('responseData', responseData);
             res.json(responseData)
         }
-    });
-
+    });*/
 })
 
 // define port
